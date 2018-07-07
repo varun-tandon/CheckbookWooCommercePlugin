cf5e58980b2ea67fda167e3cec24ccc3c42759bf
Varun Tandon <vrntandon@gmail.com>
2018-07-06 17:53:53 -0700

    Functional wc_callback with wp_post (not tested much)

--- callback.php
+++ callback.php
@@ -1,4 +1,5 @@
 <?php
+require($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
 
 function checkbook_io_start_session(){
   if (!session_id())
@@ -21,22 +22,29 @@ if(!$authorization_code)
 else
 {
   //POST for token exchange
-  $url = $baseURL . '/oauth/token';
-  $handle = curl_init($url);
-  $data = array(
-    'client_id' => $client_id,
-    'grant_type' => 'authorization_code',
-    'scope' => 'check',
-    'code' => $authorization_code,
-    'redirect_uri' => $redirect_uri,
-    'client_secret' => $client_secret
-  );
-  curl_setopt($handle, CURLOPT_POST, true);
-  $test = curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
-  $test = curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
-  $resp = curl_exec($handle);
-  $formattedData = json_decode($resp, true);
-  //Pass bearer token into session
+  $data = "client_id=". $client_id ."&grant_type=authorization_code&scope=check&code=".$authorization_code."&redirect_uri=".$redirect_uri."&client_secret=". $client_secret;
+  error_log($data);
+  $response = wp_remote_post( $baseURL . "/oauth/token", array(
+    'method' => 'POST',
+    'timeout' => 30,
+    'redirection' => 10,
+    'httpversion' => '1.1',
+    'blocking' => true,
+    'headers' => array(
+      'Content-Type' => 'application/x-www-form-urlencoded'
+    ),
+    'body' => $data,
+    'cookies' => array()
+  )
+);
+
+if ( is_wp_error( $response ) ) {
+  $error_message = $response->get_error_message();
+  echo "Something went wrong: $error_message";
+} else {
+  $response = $response['body'];
+  $formattedData = json_decode($response, true);
+  error_log(print_r($response, true));
   $_SESSION['bearerToken'] = $formattedData['access_token'];
   $_SESSION['authorized'] = "true";
 
@@ -47,6 +55,38 @@ else
   window.parent.document.getElementById("txtHint").innerHTML = "<p style=\"color:green;\"> Authorization complete. You are now ready to make a payment via Checkbook. </p>";
   window.parent.modal.close();
   </script>';
+}
+
+
+
+
+
+  // $url = $baseURL . '/oauth/token';
+  // $handle = curl_init($url);
+  // $data = array(
+  //   'client_id' => $client_id,
+  //   'grant_type' => 'authorization_code',
+  //   'scope' => 'check',
+  //   'code' => $authorization_code,
+  //   'redirect_uri' => $redirect_uri,
+  //   'client_secret' => $client_secret
+  // );
+  // curl_setopt($handle, CURLOPT_POST, true);
+  // $test = curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+  // $test = curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+  // $resp = curl_exec($handle);
+  // $formattedData = json_decode($resp, true);
+  // //Pass bearer token into session
+  // $_SESSION['bearerToken'] = $formattedData['access_token'];
+  // $_SESSION['authorized'] = "true";
+  //
+  // echo '
+  // <h1> Payment Complete. Please close this window if it does not do so automatically </h1>
+  // <script>
+  // alert("Authorization complete. Please close this window if it does not close automatically.");
+  // window.parent.document.getElementById("txtHint").innerHTML = "<p style=\"color:green;\"> Authorization complete. You are now ready to make a payment via Checkbook. </p>";
+  // window.parent.modal.close();
+  // </script>';
 
 }
 ?>
